f99ead3fc8a925c81c96597c6d0ef779509b4a56
shanhe <youngvilt@gmail.com>
2020-08-30 22:39:55 +0800

    LocalDateTimeDemo.java

---
+++ src/main/java/show/shanhe/interview/time/LocalDateTimeDemo.java
@@ -0,0 +1,10 @@
+package show.shanhe.interview.time;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTimeDemo {
+
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now());
+    }
+}
